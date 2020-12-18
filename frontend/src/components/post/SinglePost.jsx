40049d58b2b6790d3c53e979a7109ebbc3ac1a8f
Farhad Hossain <farhadhossain0085@gmail.com>
2020-12-19 02:20:34 +0600

    Question Picture showing nicely

--- frontend/src/components/post/SinglePost.jsx
+++ frontend/src/components/post/SinglePost.jsx
@@ -1,30 +1,37 @@
 import React from 'react'
-import { Heading, Text, Box, List, ListItem, ListIcon } from "@chakra-ui/react";
+import { Heading, Text, Box, List, ListItem, ListIcon, Image, Wrap, WrapItem } from "@chakra-ui/react";
 import { CheckCircleIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
+import Loading from '../helpers/Loading';
 
 const SinglePost = ({ post }) => {
 
     return (
-        <Box key={post.id} boxShadow="outline" my={6} p="6" rounded="md" bg="white">
+        <Box key={post.id} boxShadow="md" my={6} p="6" rounded="md" bg="white">
             <Heading size="sm" mb={2}>{post.title}</Heading>
             <hr />
             <Text mt={2}>{post.body}</Text>
             <Heading size="sm" my={2}>Attachments</Heading>
-            <hr/>
-            <List mt={2} spacing={3}>
+            <Wrap>
                 {post.questions.map(question => {
                     return question.pictures.map(picture => {
                         return (
-                            <ListItem key={picture.id}>
-                                <ListIcon as={CheckCircleIcon} color="green.500" />
-                                <Link to={`/picture/${picture.picture}`}>
-                                    {picture.picture}
-                                </Link>
-                            </ListItem>
+                            <WrapItem key={picture.id}>
+                                <Box rounded="md" boxShadow="md">
+                                    <Image
+                                        boxSize="100px"
+                                        objectFit="cover"
+                                        src={picture.picture}
+                                        alt={picture.picture}
+                                        fallback={<Loading />}
+                                    />
+                                </Box>
+                            </WrapItem>
                         )
                     })
                 })}
+            </Wrap>
+            <List mt={2} spacing={3}>
                 {post.books.map(book => {
                     return (
                         <ListItem key={book.id}>
