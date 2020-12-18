492dfd30803490c4d49efcedc5a10c5b16a7a7e6
Farhad Hossain <farhadhossain0085@gmail.com>
2020-12-19 02:49:38 +0600

    Image lightbox added

--- frontend/src/components/post/SinglePost.jsx
+++ frontend/src/components/post/SinglePost.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Heading, Text, Box, List, ListItem, ListIcon, Image, Wrap, WrapItem } from "@chakra-ui/react";
 import { CheckCircleIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
 import Loading from '../helpers/Loading';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css'; 
+
 
 const SinglePost = ({ post }) => {
+
+    const images = []
+
+    post.questions.map(question => question.pictures.map(picture => images.push(picture)))
+
+    const [isOpen, setIsOpen] = useState(false)
+    const [photoIndex, setPhotoIndex] = useState(0)
+
+
+    const imageOnClick = (index) => {
+        setPhotoIndex(index)
+        setIsOpen(true)
+    }
 
     return (
         <Box key={post.id} boxShadow="md" my={6} p="6" rounded="md" bg="white">
@@ -14,11 +30,12 @@ const SinglePost = ({ post }) => {
             <Heading size="sm" my={2}>Attachments</Heading>
             <Wrap>
                 {post.questions.map(question => {
-                    return question.pictures.map(picture => {
+                    return question.pictures.map((picture, index) => {
                         return (
                             <WrapItem key={picture.id}>
                                 <Box rounded="md" boxShadow="md">
                                     <Image
+                                        onClick={() => imageOnClick(index)}
                                         boxSize="100px"
                                         objectFit="cover"
                                         src={picture.picture}
@@ -31,6 +48,17 @@ const SinglePost = ({ post }) => {
                     })
                 })}
             </Wrap>
+            {isOpen && (
+                <Lightbox
+                    mainSrc={images[photoIndex].picture}
+                    nextSrc={images[(photoIndex + 1) % images.length].picture}
+                    prevSrc={images[(photoIndex + images.length - 1) % images.length].picture}
+                    onCloseRequest={() => setIsOpen(false)}
+                    onMovePrevRequest={() => setPhotoIndex((photoIndex + images.length - 1) % images.length)}
+                    onMoveNextRequest={() => setPhotoIndex((photoIndex + 1) % images.length)}
+                />
+            )}
+
             <List mt={2} spacing={3}>
                 {post.books.map(book => {
                     return (
