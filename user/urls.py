a2115f5d4950a4f1994a6aac44ff8bc46fd156d1
Farhad Hossain <farhadhossain0085@gmail.com>
2020-12-21 01:32:18 +0600

    Reset password view added

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,11 @@
 from user.views import CustomConfirmEmailView, RegistrationView
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_auth.views import PasswordResetConfirmView
+
 
 urlpatterns = [
+
+    path('password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('', include('rest_auth.urls')),
     path('register/account-confirm-email/<key>/', CustomConfirmEmailView.as_view(), name='account_confirm_email'),
     path('register/', RegistrationView.as_view()),
