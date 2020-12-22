e407b90a14be9926e4bbd77dd2a42b01bb64185a
Farhad Hossain <farhadhossain0085@gmail.com>
2020-12-23 03:09:16 +0600

    removed duplicated course and add composers only for development

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,12 +2,18 @@ import {createStore, compose, applyMiddleware} from 'redux'
 import rootReducer from "./reducers/rootReducer";
 import thunk from 'redux-thunk'
 
+let composers = [applyMiddleware(thunk)]
+
+if (process.env.REACT_APP_ENVIRONMENT === 'development'){
+    if (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__){
+        composers.push(window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__())
+    }
+}
 
 const store = createStore(
     rootReducer,
     compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        ...composers
     )
 )
 
